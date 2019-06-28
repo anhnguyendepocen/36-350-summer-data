7c2f63dfe0dabe0e57c2303ab1983d3671787350
benjaminleroy <benjaminleroyusa@gmail.com>
2019-06-28 11:21:44 -0400

    endgame dataset

---
+++ Week1/endgame_scrape.R
@@ -0,0 +1,30 @@
+#https://www.analyticsvidhya.com/blog/2017/03/beginners-guide-on-web-scraping-in-r-using-rvest-with-hands-on-knowledge/
+library(rvest)
+library(tidyverse)
+
+url <- "https://transcripts.fandom.com/wiki/Avengers:_Endgame"
+
+# tidyverse way:
+endgame <- url %>%
+            read_html %>%
+            html_nodes("p") %>%
+            html_text %>%
+            data.frame
+
+write.table(endgame,
+            file = "~/Desktop/endgame.txt",
+            row.names=F,
+            quote=F)
+
+
+# non-tidyverse way
+webpage <- read_html(url)
+endgame_nodes <- html_nodes(webpage, "p")
+endgame_text <- html_text(endgame_nodes)
+endgame <- data.frame(endgame_text)
+
+
+write.table(endgame,
+            file = "~/Desktop/endgame.txt",
+            row.names=F,
+            quote=F)
