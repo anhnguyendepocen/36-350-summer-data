7e165fa66659b85e3ea8b212a00f757df225249c
benjaminleroy <benjaminleroyusa@gmail.com>
2019-07-20 14:37:32 -0400

    better parameter acceptance in load_my_files helper function

--- Week5/load_my_files_function.R
+++ Week5/load_my_files_function.R
@@ -1,14 +1,14 @@
 
 #' basic load_all style function for those writing a package without package structure.
 #'
-#' @param dir 
+#' @param dir (make sure it has a "/" in it)
 #'
 #' @return
 #' @export
 #'
 #' @examples
-load_my_files <- function(dir = ".") {
-  R_files <- list.files("R/")
+load_my_files <- function(dir = "./") {
+  R_files <- list.files(paste0(dir,"R/"))
   
   for (r_file in R_files){
     source(paste0("R/", r_file))
